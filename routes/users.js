1ed41b6861c0ecc6e77d9fa4bbe79f69e8b03337
chloelo <chloelo925@gmail.com>
2022-05-08 17:39:44 +0800

    add user resetPassword

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const usersController = require("../controllers/user.controller");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/getAllUsers', usersController.getUsers);
+router.post('/addUser', usersController.createUser);
+router.patch('/resetPassword/:id', usersController.resetUserPassword);
 
 module.exports = router;
