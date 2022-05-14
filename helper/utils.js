d87f14e18a1d64d6eb1b9c30b04e0eda748f4b09
East lin <east_lin@symphox.net>
2022-05-14 18:31:54 +0800

    feat：add update user info

---
+++ helper/utils.js
@@ -0,0 +1,16 @@
+const filterParams = (obj) => {
+  let _newObj = {};
+
+  for (let key in obj) {
+    // 過濾資料
+    if ((obj[key] === 0 || obj[key] === false || obj[key]) && obj[key].toString().replace(/(^\s*)|(\s*$)/g, '') !== '') {
+      _newObj[key] = obj[key];
+    }
+  }
+
+  return _newObj;
+}
+
+module.exports = {
+  filterParams,
+};
